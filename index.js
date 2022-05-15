c774abdb377b2aa36dfd2a3e99fa601fd7b96c89
mellonges <b89185236417@mail.ru>
2022-05-16 00:46:32 +0300

    fix online

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const fs = require('fs');
+const fs = require("fs")
 const port = process.env.PORT || 3000;
 let userCounter = 0;
 app.get('/', (req, res) => {
@@ -9,26 +9,26 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+  userCounter += 1
+  console.log(userCounter)
+  io.emit("change online", userCounter)
   const rawdata = fs.readFileSync('lastMessage.json');
   const message = JSON.parse(rawdata);
-  console.log(message)
-  io.emit("chat message", message.lastMessage)
+  socket.emit("chat message", message.lastMessage)
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
     let data = JSON.stringify({"lastMessage": msg});
     fs.writeFileSync('lastMessage.json', data);
   });
-});
-io.on('connection', (socket) => {
-  userCounter++
-  io.emit("change online", userCounter)
-  console.log(`a user connected. All users: ${userCounter}`);
-  socket.on('disconnect', () => {
-    userCounter--
+  socket.on('disconnect', _ => {
+    userCounter -= 1
+    console.log(userCounter)
     io.emit("change online", userCounter)
     console.log(`user disconnected. All users: ${userCounter}`);
   });
 });
+
+
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
